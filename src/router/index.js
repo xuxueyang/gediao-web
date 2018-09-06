8ac7d0d10f71225abc927650a5e55c349a3457ba
xuxy <xuxy@fengyuntec.com>
2018-09-06 15:07:32 +0800

    测试一些特殊样式js功能css

--- src/router/index.js
+++ src/router/index.js
@@ -38,49 +38,8 @@ export const constantRouterMap = [
   { path: '/home', component: () => import('@/views/home/index'), hidden: true },
   { path: '/qlh', component: () => import('@/views/qlh/index'), hidden: true },
 
-  {
-    path: '/dashboard',
-    component: Layout,
-    hidden: true,
-    children: [{
-      path: 'dashboard',
-      component: () => import('@/views/dashboard/index')
-    }]
-  },
-  {
-    path: '/example',
-    component: Layout,
-    redirect: '/example/table',
-    name: 'Example',
-    meta: { title: 'Example', icon: 'example' },
-    children: [
-      {
-        path: 'table',
-        name: 'Table',
-        component: () => import('@/views/table/index'),
-        meta: { title: 'Table', icon: 'table' }
-      },
-      {
-        path: 'tree',
-        name: 'Tree',
-        component: () => import('@/views/tree/index'),
-        meta: { title: 'Tree', icon: 'tree' }
-      }
-    ]
-  },
+  // { path: '/effects/type-effects', component: () => import('@/views/effects/type-effects/index'), hidden: true }
 
-  {
-    path: '/form',
-    component: Layout,
-    children: [
-      {
-        path: 'index',
-        name: 'Form',
-        component: () => import('@/views/form/index'),
-        meta: { title: 'Form', icon: 'form' }
-      }
-    ]
-  }
 
   // { path: '*', redirect: '/404', hidden: true }
 ]
---
+++ src/views/effects/type-effects/index.vue
@@ -0,0 +1,95 @@
+<template>
+<div>
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css" media="screen" type="text/css" />
+  
+
+    <div class="code-editor">
+        <span class="control"></span>
+        <span class="control"></span>
+        <span class="control"></span>
+        <pre class="numbers linenums prettyprint prettyprinted">
+          <ol class="linenums">
+            <li class="L0">
+              <code class="css">
+                <span class="pln">describe</span>
+                <span class="pun">(</span>
+                <span class="str">'Connection'</span>
+                <span class="pun">,</span>
+                <span class="pln"> </span>
+                <span class="kwd">function</span>
+                <span class="pun">(){</span>
+              </code>
+            </li>
+            <li class="L1">
+              <code class="css">
+                <span class="pln">  </span>
+                <span class="kwd">var</span>
+                <span class="pln"> db </span>
+                <span class="pun">=</span>
+                <span class="pln"> </span>
+                <span class="kwd">new</span>
+                <span class="pln"> </span>
+                <span class="typ">Connection</span>
+              </code>
+            </li>
+            <li class="L2">
+              <code class="css">
+                <span class="pln">    </span>
+                <span class="pun">,</span>
+                <span class="pln"> tobi </span>
+                <span class="pun">=</span>
+                <span class="pln"> </span>
+                <span class="kwd">new</span>
+                <span class="pln"> </span>
+                <span class="typ">User</span>
+                <span class="pun">(</span>
+                <span class="str">'tobi'</span>
+                <span class="pun">)</span>
+              </code>
+            </li>
+            <li class="L3">
+              <code class="css">
+                <span class="pln">    </span>
+                <span class="pun">,</span>
+                <span class="pln"> loki </span>
+                <span class="pun">=</span>
+                <span class="pln"> </span>
+                <span class="kwd">new</span>
+                <span class="pln"> </span>
+                <span class="typ">User</span>
+                <span class="pun">(</span>
+                <span class="str">'loki'</span>
+                <span class="pun">)</span>
+              </code>
+            </li>
+            <li class="L4">
+              <code class="css">
+                <span class="pln">    </span>
+                <span class="pun">,</span>
+                <span class="pln"> jane </span>
+                <span class="pun">=</span>
+                <span class="pln"> </span>
+                <span class="kwd">new</span>
+                <span class="pln"> </span>
+                <span class="typ">User</span>
+                <span class="pun">(</span>
+                <span class="str">'jane'</span>
+                <span class="pun">);</span>
+              </code>
+            </li>
+          </ol>
+        </pre>
+      </div>
+  
+</div>
+
+</template>
+<script>
+import jquery from "./jquery.js";
+import prettify from "./prettify.js";
+export default {
+  
+}
+</script>
