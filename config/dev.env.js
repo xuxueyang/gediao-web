524eae34339442699d23788323edc467fd952ea4
xuxy <xuxy@fengyuntec.com>
2019-05-05 14:25:30 +0800

    update

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,6 @@ const prodEnv = require('./local.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"dev"',
-  BASE_API: '"http://193.112.17.169:9999"'
+  // BASE_API: '"http://193.112.17.169:9999"'
+  BASE_API: '"http://localhost:9999"'
 })
